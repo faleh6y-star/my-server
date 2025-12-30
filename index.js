14ac466dc1ef4edd045986fea34a511192f824ca
faleh6y-star <faleh6y@gmail.com>
2025-12-30 13:36:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ const app = express();
 
 app.use(express.json());
 
+let lastSignal = null;
+
+// TradingView يرسل هنا
 app.post("/webhook", (req, res) => {
-  console.log("Signal received:", req.body);
+  lastSignal = req.body;
+  console.log("Signal received:", lastSignal);
   res.status(200).send("OK");
+});
+
+// التطبيق يطلب هنا
+app.get("/signal", (req, res) => {
+  res.json(lastSignal);
 });
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
